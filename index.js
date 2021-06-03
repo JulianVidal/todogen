00196b934883739eacabb096e690b8615a02c9d3
JulianVidal <49546430+spiderpiggy03@users.noreply.github.com>
2021-06-03 22:44:47 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+let cities, words, actions, places;
+
+document.addEventListener("click", () => {
+  main()
+})
+
+document.addEventListener("keyup", e => {
+  if (e.key == ' ') {
+    const theme = document.documentElement.getAttribute("theme") == "dark" ? "light" : "dark";
+    document.documentElement.setAttribute("theme", theme)
+  }
+})
+
+Promise.all([
+  fetch("./cities.json").then(res => res.json()).then(json => json.map(el => el.name)),
+  fetch("./common.json").then(res => res.json()).then(json => json.commonWords),
+  fetch("./actions.json").then(res => res.json()),
+  fetch("./places.json").then(res => res.json())
+])
+.then(data => {
+  [cities, words, actions, places] = data
+  main()
+})
+
+function main() {
+  let string = '';
+
+  string += pickRandom(actions) + pickRandom(cities) + "'s" + pickRandom(places)
+
+  const el = document.getElementById("text")
+  el.innerHTML = string;
+}
+
+function pickRandom(array) {return ' ' + array[Math.floor(Math.random() * array.length)]}
